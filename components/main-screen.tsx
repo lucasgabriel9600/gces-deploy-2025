6e3779ed27c7f4675e7deaaf7e0128c68fec7a05
lucasgabriel9600 <130786702+lucasgabriel9600@users.noreply.github.com>
2025-09-03 23:02:42 +0000

    Aula 03/09/2025

---
+++ components/main-screen.tsx
@@ -0,0 +1,14 @@
+const MainScreen = () => {
+    return(
+        <main>
+            <h1>
+                Exemplo de Deploy
+            </h1>
+            <p>
+                Exemplificando a entrega contínua
+            </p>
+        </main>
+    )
+}
+
+export default MainScreen;
